199c584c5a3d15440a859095933c5dfba535edb1
Sri Harsha Kappala <sriharsha.k@caratlane.com>
2018-05-23 23:30:58 +0530

    Ignore Yellow Box, isMounted deperecated warning

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
 import { AppRegistry } from 'react-native';
 import Navigator from './app/components/Navigator';
 
